6f0e8bd652756d1f819f7fc6c6e370542c7d2cbb
NT <nurbekinho@gmail.com>
2023-02-09 12:02:02 +0100

    Minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,27 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+let tgWA = window.Telegram.WebApp;
+tgWA.expand()
+
+tgWA.MainButton.textColor = "#FFFFFF";
+tgWA.MainButton.color = "#00FF00";
+
+let item = "";
+
+let btn1 = document.getElementById("button1");
+
+btn1.addEventListener("click", function() {
+    if (tgWA.MainButton.isVisible) {
+        tgWA.MainButton.hide();
+    } else {
+        tgWA.MainButton.setText("Button 1 clicked");
+        item = "1";
+        tgWA.MainButton.show();
+    }
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tgWA.sendData(item);
+});
